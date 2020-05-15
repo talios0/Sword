92b0c43b3123524fccec1621f6bc0bf605ca11cf
talios0 <20057606+talios0@users.noreply.github.com>
2020-05-15 11:30:43 -0400

    Auto Teleport back to stage

---
+++ Assets/Scripts/Misc/Teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform player;
+
+    private void FixedUpdate()
+    {
+        if (player.position.y < transform.position.y) player.transform.position = new Vector3(0, -1.015254f, 0);
+    }
+
+}
