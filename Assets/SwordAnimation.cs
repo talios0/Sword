83c0214e0160d8b935d834ca221d4991f5a3132a
talios0 <20057606+talios0@users.noreply.github.com>
2020-05-17 13:29:03 -0400

    Somewhat functional sword withdrawal

---
+++ Assets/SwordAnimation.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class SwordAnimation : MonoBehaviour
+{
+    public Transform hand;
+    public Transform hip;
+    public Transform standard;
+
+    public void SetParentToHand() {
+        transform.parent = hand;
+    }
+
+    public void SetParentToDefault() {
+        transform.parent = standard;
+    }
+
+    public void SetParentToHip() {
+        transform.parent = hip;
+    }
+
+}
